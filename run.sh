da71d0c8d01063eb8b611f45af02f53579603476
Alcadramin <hello@alca.dev>
2022-05-25 20:08:29 +0000

    Added fmt to run script

--- run.sh
+++ run.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+BOLD="\033[1m"
+NORMAL="\033[0m"
+
 if [[ -z "$1" || -z "$2" ]]
   then
-    echo "USAGE: ./run.sh <day number> <chapter number>"
-    echo "       ./run.sh 01 01"
+    echo -e "${BOLD}USAGE:${NORMAL} ./run.sh <day number> <chapter number>"
+    echo -e "       ./run.sh 01 01"
     exit 1
 fi
 
 FILE=day$1/chapter$2.lua
 
 if [ -f "$FILE" ]; then
-    echo "Running: $FILE"
+    echo -e "${BOLD}Running:${NORMAL} $FILE\n"
     lua day$1/chapter$2.lua
 else 
     echo "$FILE does not exist."
