493c15d26b92675bbe2a0d0a5a7ec0da2f687e24
Berkcan Uçan <hello@alca.dev>
2022-05-27 23:40:49 +0300

    Day01 finished

--- day01/main.lua
+++ day01/main.lua
@@ -1,26 +1,39 @@
 function openfile(filename) return assert(loadfile(filename))() end
 
-local Polyfills = openfile('./utils/common.lua')
+local Utils = openfile('./utils/common.lua')
 
-local input = './day01/input.txt'
-if not Polyfills.file_exists(input) then return {} end
+local input = './day01/sample.txt'
+if not Utils.file_exists(input) then return {} end
 
 function part01()
     local answer = 0
     local temp = -1
 
-
     for line in io.lines(input) do
-        number = tonumber(line)
+        local number = tonumber(line)
         if temp >= 0 and number > temp then answer = answer + 1 end
         temp = number
     end
 
-    print('Answer:', answer)
+    print('Part01 Answer:', answer)
 end
 
 function part02()
+    local answer = -3
+    local i0 = 0
+    local i1 = 0
+    local i2 = 0
 
+    for line in io.lines(input) do
+        local number = tonumber(line)
+        if (number + i1 + i2) > (i0 + i1 + i2) then answer = answer + 1 end
+        i0 = i1
+        i1 = i2
+        i2 = number
+    end
+
+    print('Part02 Answer:', answer)
 end
 
 part01()
+part02()
