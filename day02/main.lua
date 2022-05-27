5ab0a99273f9fd174993437aaabc6e8682a2462f
Berkcan Uçan <hello@alca.dev>
2022-05-28 00:21:47 +0300

    Added Day02 - Part01

--- day02/main.lua
+++ day02/main.lua
@@ -2,11 +2,28 @@ function openfile(filename) return assert(loadfile(filename))() end
 
 local Utils = openfile('./utils/common.lua')
 
-local input = './day02/sample.txt'
+local input = './day02/input.txt'
 if not Utils.file_exists(input) then return {} end
 
 function part01()
-  print('Not implemented')
+  local pos = 0
+  local depth = 0
+
+  for line in io.lines(input) do
+    if line:match('forward') then
+      pos = pos + tonumber(line:match('forward (%d+)'))
+    elseif line:match('up') then
+      depth = depth - tonumber(line:match('up (%d+)'))
+    elseif line:match('down') then
+      depth = depth + tonumber(line:match('down (%d+)'))
+    end
+  end
+
+  print('Part01 Answer:', pos*depth)
+end
+
+function part02()
+
 end
 
 part01()
